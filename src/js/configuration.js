6196df6ded49a3b91fb81367cd5f12e41e4e8c70
Ben Thomas <benjeh32@gmail.com>
2019-05-23 01:27:04 +0100

    Fixed issue where configuration wasn't properly overwriting default.

--- src/js/configuration.js
+++ src/js/configuration.js
@@ -1,11 +1,12 @@
+// Imports
+import deepmerge from 'deepmerge'
+
 // Constants
 const defaultConfiguration = {
   necessary: {
     title: 'Necessary Storage',
-    description: 'Necessary storage enables core functionality by using, for example, cookies. The website cannot function properly without this, and it can only be disabled by changing your browser preferences.',
-    handleEnabled: function () {
-      console.log('Privacy Choices: handling \'necessary\' category.')
-    }
+    description: 'Necessary storage enables core site functionality. This site cannot function properly without it, and it can only be disabled by changing your browser preferences.',
+    handleEnabled: function () {}
   },
   categories: [ ],
   language: {
@@ -13,13 +14,13 @@ const defaultConfiguration = {
       openButton: 'Privacy Choices',
       closeButton: 'Close',
       heading: 'Your privacy choices',
-      description: 'This site uses cookies. You can set your privacy choices below. Changes will take effect immediately.',
+      description: 'This site uses cookies and other web storage technologies. You can set your privacy choices below. Changes will take effect immediately.',
       acceptAllButton: 'Accept all',
       declineAllButton: 'Decline all'
     },
     prompt: {
       heading: 'Your privacy choices for this site',
-      description: 'This site uses cookies and other storage technologies.',
+      description: 'This site uses cookies and other web storage technologies.',
       acceptButton: 'Accept and continue',
       settingsButton: 'Manage choices'
     }
@@ -40,7 +41,7 @@ class PrivacyChoicesConfiguration {
     var customConfiguration = window.privacyChoicesConfiguration || { }
 
     // Merge custom into default, preferring custom values
-    this.configuration = Object.assign(defaultConfiguration, customConfiguration)
+    this.configuration = deepmerge(defaultConfiguration, customConfiguration)
 
     return this.configuration
   }
